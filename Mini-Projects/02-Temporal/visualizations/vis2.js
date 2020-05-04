622a8137efc08ff9974eec03f5d7c14b70fd3712
mkarroqe@gmail.com <mkarroqe@gmail.com>
2020-05-04 10:58:58 -0400

    done omg

--- Mini-Projects/02-Temporal/visualizations/vis2.js
+++ Mini-Projects/02-Temporal/visualizations/vis2.js
@@ -1,8 +1,8 @@
-function vis2(data, div) {
-  const margin = {top: 40, right: 10, bottom: 100, left: 45};
-  const visWidth = 1430 - margin.left - margin.right;
+function vis2(data, data2, div) {
+  const margin = {top: 40, right: 10, bottom: 100, left: 95};
+  const visWidth = 1420 - margin.left - margin.right;
   const visHeight = 600 - margin.top - margin.bottom;
-  
+
   // ------------------- svg + g ------------------
   const svg = div.append("svg")
       .attr("width", visWidth + margin.left + margin.right)
@@ -14,73 +14,63 @@ function vis2(data, div) {
       .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
   // ------------------ x values ------------------ 
-  const x_elements = d3.set(data.map(function(item) { 
+  const x_elements = d3.set(data2.map(function(item) { 
       return item.year; 
     }
   )).values();
-  // console.log(x_elements);
 
   const x = d3.scaleBand()
-      .domain(x_elements)
-      .range([0, visWidth * 0.82]);
-      // .padding(0.25);
+    .domain(x_elements)
+    .range([0, visWidth * 0.8]);
 
   const xAxis = d3.axisBottom(x);
+      // .tickPadding(10)
+      // .tickSize(0); // ticks aid readibility
   
   g.append('g')
+      .attr('transform', `translate(0,${visHeight})`)
       .call(xAxis)
-      .call(g => g.selectAll('.domain').remove())
-      .attr('y', visHeight / 2);
-      // .attr('x', -40);
-
-  // ------------------ y values ------------------ 
+      .call(g => g.selectAll('.domain').remove());
+  
   const y = d3.scaleLinear()
       .domain([0, 1]).nice()
       .range([visHeight, 0]);
   
+  // ------------------ y values ------------------ 
   const yAxis = d3.axisLeft(y).tickFormat(d3.format(".0%"));
 
   g.append("g")
       .call(yAxis)
       .call(g => g.selectAll('.domain').remove())
-    .append('text')
-      .attr('fill', 'black')
-      .attr('x', -40)
-      .attr('y', visHeight / 2)
-      .text("Amt (%)");
 
-  // -------------------- data -------------------- 
-  const purpose = d3.set(data.map(function(item) { 
-      return item.purpose; 
-    }
-  )).values();
-  const amount = d3.set(data.map(function(item) { 
-      return item.amount; 
-    }
-  )).values();
-  // console.log(amount);
-
-  const color = d3.scaleOrdinal()
-    .domain(purpose)
+  // ------------------- color --------------------
+  var color = d3.scaleOrdinal()
+    .domain(data.columns.slice(1))
     .range(d3.schemeCategory10);
+  
+  // -------------------- data --------------------
+  const area = d3.area()
+    .x(d => x(d.data.year))
+    .y0(d => y(d[0]))
+    .y1(d => y(d[1]));
 
-  const series = g.selectAll('.series')
-    .data(data)
-    .join('g')
-      .attr('fill', d => color(d.key))
-      // .attr('class', 'amount')
-    .selectAll('rect')
-    .data(d => d)
-    .join('rect')
-      .attr('y', d => y(d[1]))
-      .attr('height', d => y(d[0]) - y(d[1]))
-      .attr('x', (d, i) => x(d.data.year))
-      .attr('width', x.bandwidth());
+  const series = d3.stack()
+    .keys(data.columns.slice(1))
+    .offset(d3.stackOffsetExpand)(data);
+
+  svg.append("g")
+    .attr("transform", "translate(108, 40)")
+    .selectAll("path")
+    .data(series)
+    .join("path")
+    .attr("fill", ({key}) => color(key))
+    .attr("d", area)
+    .text(({key}) => key);
 
   // -------------------- legend ------------------
   svg.append("g")
     .attr("class", "legendOrdinal")
-    .attr("transform", "translate(1200, 355)")
+    .attr("transform", "translate(1180, 300)")
     .attr("font-size", 9);
 
   var legendOrdinal = d3.legendColor()
@@ -94,11 +84,10 @@ function vis2(data, div) {
   // --------------------- title ------------------
   g.append('text')
       .attr('class', 'title')
-      .attr('x', visWidth / 2 - 200)
-      .attr('y', -45)
+      .attr('x', visWidth / 2 - 300)
+      .attr('y', -35)
       .attr('font-size', 20)
       .text('Top 10 Coalesced Purposes of Donations')
-  
 }
 
 
