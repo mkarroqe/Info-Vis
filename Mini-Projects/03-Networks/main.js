28e49aa6eedd61307e43beecb79f397e6e46942d
mkarroqe@gmail.com <mkarroqe@gmail.com>
2020-05-11 03:59:32 -0400

    vis 2 mess

--- Mini-Projects/03-Networks/main.js
+++ Mini-Projects/03-Networks/main.js
@@ -1,10 +1,43 @@
-// Load the datasets and call the functions to make the visualizations
-
+// i know vis2 is a yikes but it is what it is
 Promise.all([
-  d3.csv('data/vis1Data.csv', d3.autoType),
-  d3.csv('data/vis2Data.csv', d3.autoType),
-  d3.csv('data/vis2datafull.csv', d3.autoType)
-]).then(([data1, data2, data3]) => {
-  vis1(data1, d3.select('#vis1'));
-  vis2(data3, data2, d3.select('#vis2'));
+  d3.csv('data/vis1.csv', d3.autoType),
+
+  d3.csv('data/vis2-1.csv', d3.autoType),
+  d3.csv('data/vis2-2.csv', d3.autoType),
+  d3.csv('data/vis2-3.csv', d3.autoType),
+  d3.csv('data/vis2-4.csv', d3.autoType),
+  d3.csv('data/vis2-5.csv', d3.autoType),
+  d3.csv('data/vis2-6.csv', d3.autoType),
+  d3.csv('data/vis2-7.csv', d3.autoType),
+  d3.csv('data/vis2-8.csv', d3.autoType),
+  d3.csv('data/vis2-9.csv', d3.autoType),
+  d3.csv('data/vis2-10.csv', d3.autoType),
+  
+]).then(([
+	data1, 
+
+	data2_1, 
+	data2_2, 
+	data2_3, 
+	data2_4, 
+	data2_5, 
+	data2_6, 
+	data2_7, 
+	data2_8, 
+	data2_9, 
+	data2_10
+
+]) => {
+	vis1(data1, d3.select('#vis1'));
+	vis2(
+		data2_1, 
+		data2_2, 
+		data2_3, 
+		data2_4, 
+		data2_5, 
+		data2_6, 
+		data2_7, 
+		data2_8, 
+		data2_9, 
+		data2_10, d3.select('#vis2'));
 });
--- Mini-Projects/03-Networks/visualizations/vis2.js
+++ Mini-Projects/03-Networks/visualizations/vis2.js
@@ -1,5 +1,5 @@
-function vis2(data, data2, div) {
-  const margin = {top: 40, right: 10, bottom: 100, left: 95};
+function vis2(data2_1, data2_2, data2_3, data2_4, data2_5, data2_6, data2_7, data2_8, data2_9, data2_10, div) {
+  const margin = {top: 40, right: 100, bottom: 100, left: 55};
   const visWidth = 1420 - margin.left - margin.right;
   const visHeight = 600 - margin.top - margin.bottom;
 
@@ -14,63 +14,112 @@ function vis2(data, data2, div) {
       .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
   // ------------------ x values ------------------ 
-  const x_elements = d3.set(data2.map(function(item) { 
-      return item.year; 
+  const x_elements = d3.set(data2_1.map(function(item) { 
+      return item.donor; 
     }
   )).values();
 
-  const x = d3.scaleBand()
-    .domain(x_elements)
-    .range([0, visWidth * 0.8]);
+  const x = d3.scalePoint()
+      .domain(x_elements)
+      .range([0, visWidth / 1.01]);
 
-  const xAxis = d3.axisBottom(x);
-      // .tickPadding(10)
-      // .tickSize(0); // ticks aid readibility
+  // the radius of the pie charts
+  const outerRadius = x.step() / 4.1;
   
+  const xAxis = d3.axisBottom(x)
+      .tickSize(0)
+
   g.append('g')
-      .attr('transform', `translate(0,${visHeight})`)
-      .call(xAxis)
-      .call(g => g.selectAll('.domain').remove());
-  
-  const y = d3.scaleLinear()
-      .domain([0, 1]).nice()
-      .range([visHeight, 0]);
+    .attr('transform', `translate(0,${(visHeight / 2) + outerRadius + 5})`)
+    .call(xAxis)
+    .call(g => g.selectAll('.domain').remove());
   
   // ------------------ y values ------------------ 
-  const yAxis = d3.axisLeft(y).tickFormat(d3.format(".0%"));
+  const y_elements = d3.set(data2_1.map(function(item) { 
+      return item.recipient; 
+    }
+  )).values();
+
+  const y = d3.scalePoint()
+      .domain(y_elements)
+      .range([visHeight, 0]);
+
+  // y-axis
+  const yAxis = d3.axisLeft(y)
+      .tickPadding(50)
+      .tickSize(0);
 
   g.append("g")
       .call(yAxis)
       .call(g => g.selectAll('.domain').remove())
 
   // ------------------- color --------------------
-  var color = d3.scaleOrdinal()
-    .domain(data.columns.slice(1))
-    .range(d3.schemeCategory10);
+  const purposes = ["Air transport", "Industrial development", "Power generation/non-renewable sources", "Rail transport", "RESCHEDULING AND REFINANCING"]
+
+  const color = d3.scaleOrdinal()
+    .domain(purposes)
+    .range(d3.schemeTableau10);
   
   // -------------------- data --------------------
-  const area = d3.area()
-    .x(d => x(d.data.year))
-    .y0(d => y(d[0]))
-    .y1(d => y(d[1]));
+  
+  // ---------- 1. INDIA ----------
+  // const amounts = d3.set(data2_1.map(function(item) { 
+  //     return item.amount; 
+  //   }
+  // )).values();
 
-  const series = d3.stack()
-    .keys(data.columns.slice(1))
-    .offset(d3.stackOffsetExpand)(data);
+  // // TODO: figure out why amount values aren't coming up
+  // const pie = d3.pie()
+  //     .value(d => 2)
+  //     // .value(d => d.amount);
 
-  svg.append("g")
-    .attr("transform", "translate(108, 40)")
-    .selectAll("path")
-    .data(series)
-    .join("path")
-    .attr("fill", ({key}) => color(key))
-    .attr("d", area)
-    .text(({key}) => key);
+  // const arc = d3.arc()
+  //     .innerRadius(0)
+  //     .outerRadius(outerRadius);
+
+  // const pieGroups = g.selectAll('.pieGroup')
+  //   .data(data2_1)
+  //   .join('g')
+  //     .attr('class', 'pieGroup')
+  //     .attr('transform', d => `translate(${x(d.donor)},${visHeight / 2})`);
+
+  // pieGroups.selectAll('path')
+  //   .data(d => pie(d.coalesced_purpose_name))
+  //   .join('path')
+  //     .attr('d', d => arc(d))
+  //     .attr('fill', d => color(d.data.coalesced_purpose_name))
+
+  // ---------- 2. THAILAND ----------
+  const amounts2 = d3.set(data2_2.map(function(item) { 
+      return item.amount; 
+    }
+  )).values();
+
+  // TODO: figure out why amount values aren't coming up
+  const pie2 = d3.pie()
+      .value(d => 2)
+      // .value(d => d.amount);
+
+  const arc2 = d3.arc()
+      .innerRadius(0)
+      .outerRadius(outerRadius);
+
+  const pieGroups2 = g.selectAll('.pieGroup')
+    .data(data2_2)
+    .join('g')
+      .attr('class', 'pieGroup')
+      .attr('transform', d => `translate(${x(d.donor)},${visHeight / 2})`);
+
+  pieGroups2.selectAll('path')
+    .data(d => pie2(d.coalesced_purpose_name))
+    .join('path')
+      .attr('d', d => arc2(d))
+      .attr('fill', d => color(d.data.coalesced_purpose_name))
 
   // -------------------- legend ------------------
   svg.append("g")
     .attr("class", "legendOrdinal")
-    .attr("transform", "translate(1180, 300)")
+    .attr("transform", "translate(-10, 375)")
     .attr("font-size", 9);
 
   var legendOrdinal = d3.legendColor()
@@ -84,10 +133,10 @@ function vis2(data, data2, div) {
   // --------------------- title ------------------
   g.append('text')
       .attr('class', 'title')
-      .attr('x', visWidth / 2 - 300)
+      .attr('x', visWidth / 2 - 170)
       .attr('y', -35)
       .attr('font-size', 20)
-      .text('Top 10 Coalesced Purposes of Donations')
+      .text('Top 5 Coalesced Donation Purposes across Countries')
 }
 
 
