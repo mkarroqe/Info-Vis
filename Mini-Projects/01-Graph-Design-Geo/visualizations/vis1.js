6ca17128047c25aae7a1868b0c3283e3899fad07
mkarroqe@gmail.com <mkarroqe@gmail.com>
2020-05-15 20:11:05 -0400

    done!!

--- Mini-Projects/01-Graph-Design-Geo/visualizations/vis1.js
+++ Mini-Projects/01-Graph-Design-Geo/visualizations/vis1.js
@@ -1,44 +1,76 @@
 function vis1(data, div) {
-  
-  var margin = {top: 40, right: 0, bottom: 60, left: 30};
+  var margin = {top: 40, right: 0, bottom: 160, left: 30};
     
-  var width = 1100 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+  var visWidth = 1100 - margin.left - margin.right;
+  var visHeight = 800 - margin.top - margin.bottom;
 
+  // ------------------- svg + g ------------------
   var svg = div.append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("width", visWidth + margin.left + margin.right)
+        .attr("height", visHeight + margin.top + margin.bottom);
 
+  const g = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
+  // ------------------- x-values ------------------
   var x = d3.scaleLinear()
-    .range([0, width]);
+            .range([0, visWidth]);
 
-  var color = d3.scaleSequential(d3.interpolatePRGn);
+  var extent = d3.extent(data, d => d.amount);
+  x.domain(extent);
 
+  svg.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", "translate(0," + (visHeight + 10) + ")")
+    .call(d3.axisBottom(x).tickSizeOuter(0));
+
+  g.append('text')
+      .attr('class', 'title')
+      .attr('x', visWidth / 2 - 75)
+      .attr('y', -50)
+      .attr('font-size', 14)
+      .text("Net Donation (USD)")
+
+  // ------------------- y-values ------------------
   var y = d3.scaleBand()
-    .range([height, 0])
+    .range([visHeight, 0])
     .padding(0.1);
 
+  y.domain(data.map(function(d){ 
+    return d.country; 
+  }));
 
-  y.domain(data.map(function(d) { return d.country; }));
-  x.domain(d3.extent(data, function(d) { return d.amount; }));
-  
-  var max = d3.max(data, function(d) { return d.amount; });
-  color.domain([-max, max]);
-  
   svg.append("g")
     .attr("class", "y-axis")
     .attr("transform", "translate(" + x(0) + ",0)")
     .append("line")
       .attr("y1", 0)
-      .attr("y2", height);
-  
+      .attr("y2", visHeight);
+
+  // -------------------- legend ------------------
+  var color = d3.scaleSequential()
+    .domain([-130000000000, 130000000000])
+    .interpolator(d3.interpolateRdYlBu);
+
   svg.append("g")
-    .attr("class", "x-axis")
-    .attr("transform", "translate(0," + (height + 10) + ")")
-    .call(d3.axisBottom(x).tickSizeOuter(0));
+    .attr("class", "legendLinear")
+    .attr("transform", "translate(950, 20)")
+    .attr("font-size", 9);
+
+  var legendLinear = d3.legendColor()
+    .shapeWidth(35)
+    .cells(11)
+    .orient('vertical')
+    .scale(color)
+    .labelFormat(d3.format("$,"))
+    .title("Net Donation (USD)");
+
+  svg.select(".legendLinear")
+    .call(legendLinear);
+
+  // -------------------- data ------------------
+  var max = d3.max(data, function(d) { return d.amount; });
+  color.domain(extent);
   
   var bars = svg.append("g")
     .attr("class", "bars")
@@ -58,24 +90,48 @@ function vis1(data, div) {
     .style("fill", function(d) {
       return color(d.amount)
     });
-  
+
+  // -------------------- labels ------------------
+  // x-axis
+  g.append('text')
+      .attr('class', 'title')
+      .attr('x', visWidth / 2 - 50)
+      .attr('y', 625)
+      .attr('font-size', 15)
+      .text('Net Donation (USD)');
+
+  // Countries 
   var labels = svg.append("g")
     .attr("class", "labels");
   
   labels.selectAll("text")
     .data(data)
-  .enter().append("text")
+  .enter()
+    .append("text")
     .attr("class", "bar-label")
+    .attr('font-size', 9)
+
     .attr("x", x(0))
-    .attr("y", function(d) { return y(d.country)})
-    .attr("dx", function(d) {
-      return d.amount < 0 ? 5 : -5;
+    .attr("y", function(d) { 
+      return y(d.country);
     })
+
+    .attr("dx", function(d) {
+      if (d.amount < 0) {
+        return 5;
+      }
+      else {
+        return -5;
+      }
+    })
+
     .attr("dy", y.bandwidth())
+
     .attr("text-anchor", function(d) {
       return d.amount < 0 ? "start" : "end";
     })
-    .text(function(d) { return d.country });
-    
+    .text(function(d) { 
+      return d.country;
+    });
 }
 
--- Mini-Projects/01-Graph-Design-Geo/visualizations/vis2.js
+++ Mini-Projects/01-Graph-Design-Geo/visualizations/vis2.js
@@ -1,5 +1,5 @@
 function vis2(data, geoJSON, div) {
-  const margin = {top: 120, right: 20, bottom: 0, left: 20};
+  const margin = {top: 120, right: 20, bottom: -100, left: 0};
   const visWidth = 1900 - margin.left - margin.right;
   const visHeight = 600 - margin.top - margin.bottom;
 
@@ -12,15 +12,15 @@ function vis2(data, geoJSON, div) {
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
   // ----------------- projection ----------------- 
-  const projection =  d3.geoNaturalEarth1()
-      .fitSize([visWidth / 2, visHeight / 1.2], geoJSON);
+  const projection =  d3.geoMercator()
+      .fitSize([visWidth / 2, visHeight], geoJSON);
 
   const path = d3.geoPath().projection(projection);
 
   // -------------------- legend ------------------
   var color = d3.scaleSequential()
     .domain([0, 1])
-    .interpolator(d3.interpolateGreens);
+    .interpolator(d3.interpolateRdYlBu);
 
   svg.append("g")
     .attr("class", "legendLinear")
@@ -51,9 +51,9 @@ function vis2(data, geoJSON, div) {
   // --------------------- title ------------------
   g.append('text')
       .attr('class', 'title')
-      .attr('x', visWidth / 5)
+      .attr('x', 0)
       .attr('y', -65)
       .attr('font-size', 20)
-      .text('International Donation Percentages')
+      .text('Net Donations by Country');
 }
 
